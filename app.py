7f3f99da492d6d33a5f1de85350a077197bda696
navjeevan <navjeevanbomble83@gmail.com>
2026-07-15 12:07:13 +0530

    changed port number

--- app.py
+++ app.py
@@ -131,4 +131,4 @@ def health():
     return jsonify(status="ok",redis=REDIS_OK), 200
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
